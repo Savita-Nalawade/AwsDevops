1c5262b5010ab53302395d28db15085bcd637c10
Savita_Nalawade <spnalawade2000@gmail.com>
2026-06-25 23:06:32 +0530

    adding code

---
+++ src/test/java/com/example/AwsDevOps/AwsDevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AwsDevOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
